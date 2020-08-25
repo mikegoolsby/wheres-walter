c6b983c6211e24468ca982da3f5f6422f9911fcf
Michael Goolsby <mgoolsby1091@gmail.com>
2020-08-25 18:58:30 -0400

    adds html structure for dropdown list

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,12 @@ $.ajax({
         console.log(data)
     }
 });
+
+const $seasonList = $('#season');
+
+$seasonList.options[0] = new Option('--Select--', '');
+$seasonList.options[1] = new Option('Season 1', 'Season 1');
+$seasonList.options[2] = new Option('Season 2', 'Season 2');
+$seasonList.options[3] = new Option('Season 3', 'Season 3');
+$seasonList.options[4] = new Option('Season 4', 'Season 4');
+$seasonList.options[5] = new Option('Season 5', 'Season 5');
